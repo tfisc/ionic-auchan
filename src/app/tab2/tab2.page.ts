243043aacaecb41faf347bce5bb5d6dc4a00d4b2
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 18:46:43 +0200

    jitsi

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare class JitsiMeetExternalAPI{ constructor(domain, options) };
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -9,4 +9,17 @@ export class Tab2Page {
 
   constructor() {}
 
+  ac: AppComponent = new AppComponent()
+}
+
+export class AppComponent {
+  showJitsi(){
+    const domain = 'meet.jit.si';
+    const options = {
+        roomName: 'workshopg1test',
+        heigth: 700,
+        parentNode: document.querySelector('#meet')
+    };
+    const api = new JitsiMeetExternalAPI(domain, options);
+  }
 }
