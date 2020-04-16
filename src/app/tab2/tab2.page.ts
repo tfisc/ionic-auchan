09bd12380e50fba88344c84efbec3e19cbc11b10
Thibault Fischer <thibault.fischer@epsi.fr>
2020-04-16 14:59:51 +0200

    add jitsi with capacito

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -18,7 +18,7 @@ export class Tab2Page implements OnInit {
 
   async startVideo() {
     const result = await Jitsi.joinConference({
-      roomName: 'room1', // room identifier for the conference
+      roomName: 'workshop-auchan', // room identifier for the conference
       url: 'https://meet.jit.si', // endpoint of the Jitsi Meet video bridge,
       startWithAudioMuted: true, // start with audio muted
       startWithVideoMuted: false // start with video muted
