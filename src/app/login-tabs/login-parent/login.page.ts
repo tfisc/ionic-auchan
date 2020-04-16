c08868dddbbe2beeee0272098f670c6a25146bd3
lionel <sornom.lionel@gmail.com>
2020-04-16 13:37:14 +0200

    delete login directory & centralize css code for login

--- src/app/login-tabs/login-parent/login.page.ts
+++ src/app/login-tabs/login-parent/login.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['../login-tabs.page.scss'],
 })
 export class LoginPage implements OnInit {
 
