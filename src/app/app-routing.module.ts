e29716c96b7f5e5c648035c27fe4ceda872c4d72
Thibault Fischer <thibault.fischer@epsi.fr>
2020-04-17 12:15:00 +0200

    cleaned routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   {
     path: 'accueil',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'signup',
@@ -20,33 +16,31 @@ const routes: Routes = [
     loadChildren: () => import('./etudiant/etudiant.module').then(m => m.EtudiantModule)
   },
   {
-    path: 'jitsi',
-    loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule)
-  },
-  {
     path: 'enfant',
     loadChildren: () => import('./enfant/enfant.module').then(m => m.EnfantModule)
   },
   {
-    path: 'notation',
-    loadChildren: () => import('./notation/notation.module').then(m => m.NotationModule)
-  },
-  {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
-  },
-  {
-    path: 'login-student',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
-  },
-  {
-    path: 'login-child',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+    children: [{
+      path: 'parent',
+      loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+    },
+    {
+      path: 'student',
+      loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+    },
+    {
+      path: 'child',
+      loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+    },
+
+    ]
   },
   {
     path: 'parents',
     loadChildren: () => import('./tabsParents/tabsParents.module').then(m => m.TabsParentsPageModule)
   },
+  { path: '**', redirectTo: '/accueil', pathMatch: 'full' },
 
 ];
 @NgModule({
