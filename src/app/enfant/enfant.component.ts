6902da0bf51d50f8403292c4937c1def0f8bf110
lionel <sornom.lionel@gmail.com>
2020-04-17 09:18:47 +0200

    add variables to enfant.component.ts

--- src/app/enfant/enfant.component.ts
+++ src/app/enfant/enfant.component.ts
@@ -9,6 +9,12 @@ export class EnfantComponent implements OnInit {
 
   constructor() { }
   prochainCours = false;
+  matiere = 'Math';
+  imageEtudiantSrc = '../../assets/thor.jpg';
+  nomEtudiant = 'Nom étudiant';
+  diplomeEtudiant = 'Master 2 physique';
+  ecoleEtudiant = 'Université de montpellier';
+  descriptionEtudiant = 'Je sais pas faire de maths ni d\'anglais, j\'étais en classe de 3ème avec Martos.';
   ngOnInit() { }
 
 }
